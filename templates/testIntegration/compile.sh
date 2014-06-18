53e6d0a522bc328c5e7731367dad6628348d3b7b
arnaudgaudin <agaudin.lesechos@gmail.com>
2014-06-18 11:17:30 +0200

    Ajout premailer au compilateur

--- templates/testIntegration/compile.sh
+++ templates/testIntegration/compile.sh
@@ -1,4 +1,7 @@
 echo "Hi, $USER!"
 
-#compilation des fichiers LESS
+#compile less file
 lessc ${PWD}/theme/less/style.less ${PWD}/theme/css/style.css
+
+#inline style in html
+premailer ${PWD}/newsletter.html > ${PWD}/newsletter_inline.html
