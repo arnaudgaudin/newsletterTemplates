280e727f9957519962c68f8330e6e78154e34514
arnaudgaudin <agaudin.lesechos@gmail.com>
2014-06-18 10:31:12 +0200

    Création d'un fichier de compilation

---
+++ templates/testIntegration/compile.sh
@@ -0,0 +1,4 @@
+echo "Hi, $USER!"
+
+#compilation des fichiers LESS
+lessc /data/newsletters/newsletterTemplates/templates/testIntegration/theme/less/style.less /data/newsletters/newsletterTemplates/templates/testIntegration/theme/css/style.css
