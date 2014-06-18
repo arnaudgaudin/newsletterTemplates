2984636ab0cb15084a1c53ab0117f10d5cbf8948
arnaudgaudin <agaudin.lesechos@gmail.com>
2014-06-18 10:41:32 +0200

    Compilation relative au dossier courant

--- templates/testIntegration/compile.sh
+++ templates/testIntegration/compile.sh
@@ -1,4 +1,4 @@
 echo "Hi, $USER!"
 
 #compilation des fichiers LESS
-lessc /data/newsletters/newsletterTemplates/templates/testIntegration/theme/less/style.less /data/newsletters/newsletterTemplates/templates/testIntegration/theme/css/style.css
+lessc ${PWD}/theme/less/style.less ${PWD}/theme/css/style.css
