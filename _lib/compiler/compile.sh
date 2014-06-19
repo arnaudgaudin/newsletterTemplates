117b92957ac24b9b289ff6579d47a5f6bdc6e40a
arnaudgaudin <agaudin.lesechos@gmail.com>
2014-06-19 14:57:04 +0200

    Suppression tests obsolètes

--- _lib/compiler/compile.sh
+++ _lib/compiler/compile.sh
@@ -5,13 +5,9 @@ echo "inlining newsletter.html ..."
 #compile less file
 lessc ${PWD}/theme/less/style.less ${PWD}/theme/css/style.css
 
-#appel js de compilation premailer
-# sudo ${PWD}/../../_lib/compiler/premailer.rb
-
 #inline style in html
 #doc premailer: http://rubydoc.info/gems/premailer/1.8.2/Premailer
 # UTILISER IMPÉRATIVEMENT AVEC LE PARSER NOKOGIRI
 premailer -v ${PWD}/newsletter.html > ${PWD}/newsletter-inline.html
-# /usr/local/lib/node_modules/swig-email-templates/bin/swig-email-templates.js render newsletter.html -r ${PWD} > ${PWD}/newsletter-inline2.html
 
 echo "That's all folks !"
