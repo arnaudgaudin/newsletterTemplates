d2689b537fc0162696c52c15501361d13ea399ed
arnaudgaudin <agaudin.lesechos@gmail.com>
2014-06-19 15:13:56 +0200

    Ajout doc commandLine

--- _lib/compiler/compile.sh
+++ _lib/compiler/compile.sh
@@ -7,6 +7,7 @@ lessc ${PWD}/theme/less/style.less ${PWD}/theme/css/style.css
 
 #inline style in html
 #doc premailer: http://rubydoc.info/gems/premailer/1.8.2/Premailer
+# https://github.com/premailer/premailer/wiki/Premailer-Command-Line-Usage
 # UTILISER IMPÉRATIVEMENT AVEC LE PARSER NOKOGIRI
 premailer -v ${PWD}/newsletter.html > ${PWD}/newsletter-inline.html
 
