0b7710504a9fe1c05d4d61e958f1772861415eab
arnaudgaudin <agaudin.lesechos@gmail.com>
2014-06-26 17:03:48 +0200

    Rajout fichier de config et index pour serveur

---
+++ templates/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Newsletter templates</title>
+</head>
+<body>
+<h1>Newsletter templates</h1>
+<ul>
+	<li><a href="/testIntegration/newsletter.html">Test integration</a></li>
+	<li><a href="/liberaletvous/newsletter.html">Libéral et vous</a></li>
+</ul>
+</body>
+</html>
