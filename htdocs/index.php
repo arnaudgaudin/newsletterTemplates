450cc8e8f7c1c8da2d52fcd7ce54cf5ba60e6c99
arnaudgaudin <agaudin.lesechos@gmail.com>
2014-06-26 17:28:04 +0200

    Rajout encodage

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Newsletter templates</title>
+	<meta charset="utf-8">
+	<title>Newsletter templates</title>
 </head>
 <body>
 <h1>Newsletter templates</h1>
 <ul>
-	<li><a href="/templates/testIntegration/newsletter.html">Test integration</a></li>
+	<li><a href="/templates/testIntegration/newsletter.html">Test intégration</a></li>
 	<li><a href="/templates/liberaletvous/newsletter.html">Libéral et vous</a></li>
 </ul>
 </body>
