b52597241f7c3d1dd65d14540a4d19b90605388c
arnaudgaudin <agaudin.lesechos@gmail.com>
2014-06-27 15:57:37 +0200

    Utilisation du compileur avec la variable templateName pour l'url de base

--- _lib/compiler/compile.sh
+++ _lib/compiler/compile.sh
@@ -9,9 +9,8 @@ echo "inlining newsletter.html ..."
 #doc premailer: http://rubydoc.info/gems/premailer/1.8.2/Premailer
 # https://github.com/premailer/premailer/wiki/Premailer-Command-Line-Usage
 # UTILISER IMPÉRATIVEMENT AVEC LE PARSER NOKOGIRI
-# template= basename "$PWD"
-# echo "$template"
-premailer -v -e --base-url=http://newsletter-templates.expert-infos.com/templates/ ${PWD}/newsletter.html > ${PWD}/newsletter-inline.html
+templateName=$(basename "$PWD")
+premailer --verbose --entities --base-url=http://newsletter-templates.expert-infos.com/templates/$templateName --css=${PWD}/theme/css/style.css ${PWD}/newsletter.html > ${PWD}/newsletter-inline.html
 
 #Désactivation des commentaires sur les balises Posta Nova
 echo "enabling PostaNova tags ..."
