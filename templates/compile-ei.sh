fdbc0cd43ed5d518b279a66ba0fa3f50bd4729a8
arnaudgaudin <agaudin.lesechos@gmail.com>
2015-06-23 16:24:25 +0200

    Création fichier de compilation de toutes les news expertInfos

---
+++ templates/compile-ei.sh
@@ -0,0 +1,29 @@
+#compile all ExpertInfos
+echo "compiling all ExpertInfos ..."
+cd expertInfos
+./compile.sh
+cd ..
+cd expertInfos_aga
+./compile.sh
+cd ..
+cd expertInfos_av
+./compile.sh
+cd ..
+cd expertInfos_bim
+./compile.sh
+cd ..
+cd expertInfos_bimxml
+./compile.sh
+cd ..
+cd expertInfos_cartevoeux
+./compile.sh
+cd ..
+cd expertInfos_cgp
+./compile.sh
+cd ..
+cd expertInfos_hebdo
+./compile.sh
+cd ..
+cd expertInfos_hebdoxml
+./compile.sh
+cd ..
