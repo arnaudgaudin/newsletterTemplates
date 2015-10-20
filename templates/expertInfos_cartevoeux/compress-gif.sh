1ca44f4c00d75f1d8a52e9db3a1f6ea75383f54a
arnaudgaudin <agaudin.lesechos@gmail.com>
2015-10-20 12:31:01 +0200

    Rajout script de compression gif

---
+++ templates/expertInfos_cartevoeux/compress-gif.sh
@@ -0,0 +1,9 @@
+echo "Hi, $USER!"
+
+#compress gif files
+echo "compressing gif files ..."
+# gifsicle -O3 --lossy=800 -o ${PWD}/theme/images/nlcv_ambiance-800.gif ${PWD}/_sources/anim_ambiance.gif
+# gifsicle -O3 --lossy=800 -o ${PWD}/theme/images/nlcv_cadeau-800.gif ${PWD}/_sources/anim_cadeau.gif 
+# gifsicle -O3 --lossy=800 -o ${PWD}/theme/images/nlcv_nature-800.gif  ${PWD}/_sources/anim_nature.gif
+
+echo "That's all folks !"
