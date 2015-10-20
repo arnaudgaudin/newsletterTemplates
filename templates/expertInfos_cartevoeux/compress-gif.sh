44616dd6d60c88a9cb48b258830c71768de8cf94
arnaudgaudin <agaudin.lesechos@gmail.com>
2015-10-20 15:03:17 +0200

    Maj script de compression des gifs

--- templates/expertInfos_cartevoeux/compress-gif.sh
+++ templates/expertInfos_cartevoeux/compress-gif.sh
@@ -1,9 +1,12 @@
 echo "Hi, $USER!"
 
 #compress gif files
+#https://www.lcdf.org/gifsicle/
+#http://graphicdesign.stackexchange.com/questions/20908/how-to-remove-every-second-frame-from-an-animated-gif
+#Pour réduire nbre images : `seq -f "#%g" 0 2 199`
 echo "compressing gif files ..."
-# gifsicle -O3 --lossy=800 -o ${PWD}/theme/images/nlcv_ambiance-800.gif ${PWD}/_sources/anim_ambiance.gif
-# gifsicle -O3 --lossy=800 -o ${PWD}/theme/images/nlcv_cadeau-800.gif ${PWD}/_sources/anim_cadeau.gif 
-# gifsicle -O3 --lossy=800 -o ${PWD}/theme/images/nlcv_nature-800.gif  ${PWD}/_sources/anim_nature.gif
+gifsicle -v -O3 --lossy=100 --colors=128 ${PWD}/_sources/anim_cadeau.gif   -o ${PWD}/theme/images/nlcv_cadeau.gif
+gifsicle -v -O3 --lossy=800 --colors=96  ${PWD}/_sources/anim_nature.gif   -o ${PWD}/theme/images/nlcv_nature.gif
+gifsicle -v -O3 --lossy=800 --colors=128 ${PWD}/_sources/anim_ambiance.gif -o ${PWD}/theme/images/nlcv_ambiance.gif
 
 echo "That's all folks !"
