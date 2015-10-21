4121929afa86f17ad5af74dce46076255f687584
arnaudgaudin <agaudin.lesechos@gmail.com>
2015-10-21 10:49:07 +0200

    Rajout ligne pour compression nlcv_projet

--- templates/expertInfos_cartevoeux/compress-gif.sh
+++ templates/expertInfos_cartevoeux/compress-gif.sh
@@ -6,6 +6,7 @@ echo "Hi, $USER!"
 #Pour réduire nbre images : `seq -f "#%g" 0 2 199`
 echo "compressing gif files ..."
 gifsicle -v -O3 --lossy=100 --colors=128 ${PWD}/_sources/anim_cadeau.gif   -o ${PWD}/theme/images/nlcv_cadeau.gif
+gifsicle -v -O3 --lossy=200 --colors=192 ${PWD}/_sources/anim_projet.gif   -o ${PWD}/theme/images/nlcv_projet.gif
 gifsicle -v -O3 --lossy=800 --colors=96  ${PWD}/_sources/anim_nature.gif   -o ${PWD}/theme/images/nlcv_nature.gif
 gifsicle -v -O3 --lossy=800 --colors=128 ${PWD}/_sources/anim_ambiance.gif -o ${PWD}/theme/images/nlcv_ambiance.gif
 
