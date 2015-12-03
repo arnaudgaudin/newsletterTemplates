3e156b56968f8b4dbfdb98ad01dd4c1a92ce3809
arnaudgaudin <agaudin.lesechos@gmail.com>
2015-12-03 17:49:58 +0100

    Changement ordre export

--- templates/expertInfos_cartevoeux/compress-gif.sh
+++ templates/expertInfos_cartevoeux/compress-gif.sh
@@ -5,15 +5,14 @@ echo "Hi, $USER!"
 #http://graphicdesign.stackexchange.com/questions/20908/how-to-remove-every-second-frame-from-an-animated-gif
 #Pour réduire nbre images : `seq -f "#%g" 0 2 199`
 echo "compressing gif files ..."
-gifsicle -v -O3 --lossy=20  --colors=224 ${PWD}/_sources/anim_cadeau.gif   -o ${PWD}/theme/images/nlcv_cadeau.gif
-gifsicle -v -O3 --lossy=200 --colors=192 ${PWD}/_sources/anim_projet.gif   -o ${PWD}/theme/images/nlcv_projet.gif
 gifsicle -v -O3 --lossy=800 --colors=96  ${PWD}/_sources/anim_nature.gif   -o ${PWD}/theme/images/nlcv_nature.gif
+gifsicle -v -O3 --lossy=20  --colors=224 ${PWD}/_sources/anim_cadeau.gif   -o ${PWD}/theme/images/nlcv_cadeau.gif
 gifsicle -v -O3 --lossy=800 --colors=128 ${PWD}/_sources/anim_ambiance.gif -o ${PWD}/theme/images/nlcv_ambiance.gif
+gifsicle -v -O3 --lossy=200 --colors=192 ${PWD}/_sources/anim_projet.gif   -o ${PWD}/theme/images/nlcv_projet.gif
 
 echo "compressing gif files for sites ..."
-gifsicle -v -O3 --lossy=20  --colors=224 ${PWD}/_sources/anim_cadeau_site.gif   -o ${PWD}/theme/images/stcv_cadeau.gif
-gifsicle -v -O3 --lossy=50 --colors=192 ${PWD}/_sources/anim_projet_site.gif    -o ${PWD}/theme/images/stcv_projet.gif
 gifsicle -v -O3 --lossy=200 --colors=96  ${PWD}/_sources/anim_nature_site.gif   -o ${PWD}/theme/images/stcv_nature.gif
+gifsicle -v -O3 --lossy=20  --colors=224 ${PWD}/_sources/anim_cadeau_site.gif   -o ${PWD}/theme/images/stcv_cadeau.gif
 gifsicle -v -O3 --lossy=200 --colors=128 ${PWD}/_sources/anim_ambiance_site.gif -o ${PWD}/theme/images/stcv_ambiance.gif
-
+gifsicle -v -O3 --lossy=50  --colors=192 ${PWD}/_sources/anim_projet_site.gif    -o ${PWD}/theme/images/stcv_projet.gif
 echo "That's all folks !"
