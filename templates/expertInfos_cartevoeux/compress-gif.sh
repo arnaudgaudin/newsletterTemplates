e3fac5b63d8594af08bd4e80af31740c05f22cbb
arnaudgaudin <agaudin.lesechos@gmail.com>
2016-01-07 10:25:52 +0100

    Récupération Gif de Gilles et désactivation compression correspondante

--- templates/expertInfos_cartevoeux/compress-gif.sh
+++ templates/expertInfos_cartevoeux/compress-gif.sh
@@ -5,12 +5,15 @@ echo "Hi, $USER!"
 #http://graphicdesign.stackexchange.com/questions/20908/how-to-remove-every-second-frame-from-an-animated-gif
 #Pour réduire nbre images : `seq -f "#%g" 0 2 199`
 echo "compressing gif files ..."
-gifsicle -v -O3 --lossy=800 --colors=96  ${PWD}/_sources/anim_nature.gif   -o ${PWD}/theme/images/nlcv_nature.gif
+
+#Désactivation temporaire export en attendant récupération modifs Gilles dans PSD
+
+# gifsicle -v -O3 --lossy=800 --colors=96  ${PWD}/_sources/anim_nature.gif   -o ${PWD}/theme/images/nlcv_nature.gif
 gifsicle -v -O3 --lossy=20  --colors=224 ${PWD}/_sources/anim_cadeau.gif   -o ${PWD}/theme/images/nlcv_cadeau.gif
-gifsicle -v -O3 --lossy=800 --colors=128 ${PWD}/_sources/anim_ambiance.gif -o ${PWD}/theme/images/nlcv_ambiance.gif
-gifsicle -v -O3 --lossy=200 --colors=192 ${PWD}/_sources/anim_projet.gif   -o ${PWD}/theme/images/nlcv_projet.gif
-gifsicle -v -O3 --lossy=800 --colors=128 ${PWD}/_sources/anim_sommet.gif   -o ${PWD}/theme/images/nlcv_sommet.gif
-gifsicle -v -O3 --lossy=200 --colors=192 ${PWD}/_sources/anim_aeroport.gif -o ${PWD}/theme/images/nlcv_aeroport.gif
+# gifsicle -v -O3 --lossy=800 --colors=128 ${PWD}/_sources/anim_ambiance.gif -o ${PWD}/theme/images/nlcv_ambiance.gif
+# gifsicle -v -O3 --lossy=200 --colors=192 ${PWD}/_sources/anim_projet.gif   -o ${PWD}/theme/images/nlcv_projet.gif
+# gifsicle -v -O3 --lossy=800 --colors=128 ${PWD}/_sources/anim_sommet.gif   -o ${PWD}/theme/images/nlcv_sommet.gif
+# gifsicle -v -O3 --lossy=200 --colors=192 ${PWD}/_sources/anim_aeroport.gif -o ${PWD}/theme/images/nlcv_aeroport.gif
 
 echo "compressing gif files for sites ..."
 gifsicle -v -O3 --lossy=200 --colors=96  ${PWD}/_sources/anim_nature_site.gif   -o ${PWD}/theme/images/stcv_nature.gif
