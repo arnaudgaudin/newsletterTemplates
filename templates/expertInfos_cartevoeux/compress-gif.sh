54e2fe7f809efd842614fa4a106067b224b7edba
arnaudgaudin <agaudin.lesechos@gmail.com>
2016-12-26 11:48:41 +0100

    EICV : modification compression gif pour nlcv_aeroport

--- templates/expertInfos_cartevoeux/compress-gif.sh
+++ templates/expertInfos_cartevoeux/compress-gif.sh
@@ -10,16 +10,17 @@ echo "compressing gif files ..."
 
 # gifsicle -v -O3 --lossy=800 --colors=96  ${PWD}/_sources/anim_nature.gif   -o ${PWD}/theme/images/nlcv_nature.gif
 # gifsicle -v -O3 --lossy=20  --colors=224 ${PWD}/_sources/anim_cadeau.gif   -o ${PWD}/theme/images/nlcv_cadeau.gif
-gifsicle -v -O3 --lossy=800 --colors=128 ${PWD}/_sources/anim_mer_IM004706.gif -o ${PWD}/theme/images/nlcv_mer_IM004706.gif
+# gifsicle -v -O3 --lossy=800 --colors=128 ${PWD}/_sources/anim_mer_IM004706.gif -o ${PWD}/theme/images/nlcv_mer_IM004706.gif
 # gifsicle -v -O3 --lossy=800 --colors=128 ${PWD}/_sources/anim_ambiance.gif -o ${PWD}/theme/images/nlcv_ambiance.gif
 # gifsicle -v -O3 --lossy=200 --colors=192 ${PWD}/_sources/anim_projet.gif   -o ${PWD}/theme/images/nlcv_projet.gif
 # gifsicle -v -O3 --lossy=800 --colors=128 ${PWD}/_sources/anim_sommet.gif   -o ${PWD}/theme/images/nlcv_sommet.gif
 # gifsicle -v -O3 --lossy=200 --colors=192 ${PWD}/_sources/anim_aeroport.gif -o ${PWD}/theme/images/nlcv_aeroport.gif
+gifsicle -v -O3 --lossy=200 --colors=192 ${PWD}/theme/images/nlcv_aeroport_ORIG.gif -o ${PWD}/theme/images/nlcv_aeroport.gif
 
 echo "compressing gif files for sites ..."
 # gifsicle -v -O3 --lossy=200 --colors=96  ${PWD}/_sources/anim_nature_site.gif   -o ${PWD}/theme/images/stcv_nature.gif
 # gifsicle -v -O3 --lossy=20  --colors=224 ${PWD}/_sources/anim_cadeau_site.gif   -o ${PWD}/theme/images/stcv_cadeau.gif
-gifsicle -v -O3 --lossy=200 --colors=128 ${PWD}/_sources/anim_mer_site_IM004706.gif -o ${PWD}/theme/images/stcv_mer_IM004706.gif
+# gifsicle -v -O3 --lossy=200 --colors=128 ${PWD}/_sources/anim_mer_site_IM004706.gif -o ${PWD}/theme/images/stcv_mer_IM004706.gif
 # gifsicle -v -O3 --lossy=200 --colors=128 ${PWD}/_sources/anim_ambiance_site.gif -o ${PWD}/theme/images/stcv_ambiance.gif
 # gifsicle -v -O3 --lossy=50  --colors=192 ${PWD}/_sources/anim_projet_site.gif   -o ${PWD}/theme/images/stcv_projet.gif
 # gifsicle -v -O3 --lossy=200 --colors=128 ${PWD}/_sources/anim_sommet_site.gif   -o ${PWD}/theme/images/stcv_sommet.gif
