bc5e17b14df6c663f947c8037725940b3a534250
arnaudgaudin <agaudin.lesechos@gmail.com>
2017-02-16 14:37:25 +0100

    Gestion affichage nouveaux templates ftp dans index

--- htdocs/index.php
+++ htdocs/index.php
@@ -6,17 +6,32 @@
 </head>
 <body>
 <h1>Newsletter templates</h1>
+<h2>Templates gérés via GIT</h2>
+<h3>Ne pas overrider via FTP !</h3>
 <ul>
 <?php
-$templates = scandir('templates');
-if($templates != false) {
-	foreach ($templates as $template) {
-		if(strlen($template) > 2) {
-			echo '<li><a href="/templates/' . $template . '/newsletter.html">' . $template . '</a></li>';
+$templatesGit = scandir('templates');
+if($templatesGit != false) {
+	foreach ($templatesGit as $templateGit) {
+		if(strlen($templateGit) > 2) {
+			echo '<li><a href="/templates/' . $templateGit . '/newsletter.html">' . $templateGit . '</a></li>';
 		}
 	}
 }
 ?>
 </ul>
+
+<h2>Templates gérés via FTP</h2>
+<h3>Peuvent être overridés via FTP.</h3>
+<?php
+$templatesFtp = scandir('ExpertCom');
+if($templatesFtp != false) {
+	foreach ($templatesFtp as $templateFtp) {
+		if(strlen($templateFtp) > 2) {
+			echo '<li><a href="/ExpertCom/' . $templateFtp . '/newsletter.html">' . $templateFtp . '</a></li>';
+		}
+	}
+}
+?>
 </body>
 </html>
