ac7c25079e456c9352737916e61dc59b7550b41b
arnaudgaudin <agaudin.lesechos@gmail.com>
2017-12-04 12:13:31 +0100

    Avancement params GIF

--- templates/expertInfos_cartevoeux/compress-gif.sh
+++ templates/expertInfos_cartevoeux/compress-gif.sh
@@ -12,11 +12,11 @@ echo "compressing gif files ..."
 # giflossy -v -O3 --lossy=800 -k96  ${PWD}/_sources/anim_nature.gif      -o ${PWD}/theme/images/nlcv_nature.gif
 # giflossy -v -O3 --lossy=200 -k192 ${PWD}/_sources/anim_projet.gif      -o ${PWD}/theme/images/nlcv_projet.gif
 # giflossy -v -O3 --lossy=800 -k128 ${PWD}/_sources/anim_sommet.gif      -o ${PWD}/theme/images/nlcv_sommet.gif
-giflossy -v -O3 --lossy=300 -k64  ${PWD}/_sources/anim_feerique.gif    `seq -f "#%g" 0 3 139` -o ${PWD}/theme/images/nlcv_feerique.gif
-giflossy -v -O3 --lossy=600 -k192 ${PWD}/_sources/anim_mongolfiere.gif `seq -f "#%g" 0 2 99`  -o ${PWD}/theme/images/nlcv_mongolfiere.gif
-giflossy -v -O3 --lossy=800 -k128 ${PWD}/_sources/anim_neige.gif       `seq -f "#%g" 0 3 139` -o ${PWD}/theme/images/nlcv_neige.gif
-giflossy -v -O3 --lossy=600 -k192 ${PWD}/_sources/anim_ponton.gif      `seq -f "#%g" 0 3 139` -o ${PWD}/theme/images/nlcv_ponton.gif
-# giflossy -v -O3 --lossy=200 -k192 ${PWD}/_sources/anim_recette.gif       -o ${PWD}/theme/images/nlcv_recette.gif
+# giflossy -v -O3 --lossy=300 -k64  ${PWD}/_sources/anim_feerique.gif    `seq -f "#%g" 0 3 139` -o ${PWD}/theme/images/nlcv_feerique.gif
+# giflossy -v -O3 --lossy=600 -k192 ${PWD}/_sources/anim_mongolfiere.gif `seq -f "#%g" 0 2 99`  -o ${PWD}/theme/images/nlcv_mongolfiere.gif
+# giflossy -v -O3 --lossy=800 -k128 ${PWD}/_sources/anim_neige.gif       `seq -f "#%g" 0 3 139` -o ${PWD}/theme/images/nlcv_neige.gif
+# giflossy -v -O3 --lossy=600 -k192 ${PWD}/_sources/anim_ponton.gif      `seq -f "#%g" 0 3 139` -o ${PWD}/theme/images/nlcv_ponton.gif
+giflossy -v -O3 --lossy=800 -k96 ${PWD}/_sources/anim_recette.gif `seq -f "#%g" 0 2 79` -o ${PWD}/theme/images/nlcv_recette.gif
 
 
 echo "compressing gif files for sites ..."
@@ -27,11 +27,11 @@ echo "compressing gif files for sites ..."
 # giflossy -v -O3 --lossy=200 -k96  ${PWD}/_sources/anim_nature_site.gif      -o ${PWD}/theme/images/stcv_nature.gif
 # giflossy -v -O3 --lossy=50  -k192 ${PWD}/_sources/anim_projet_site.gif      -o ${PWD}/theme/images/stcv_projet.gif
 # giflossy -v -O3 --lossy=200 -k128 ${PWD}/_sources/anim_sommet_site.gif      -o ${PWD}/theme/images/stcv_sommet.gif
-giflossy -v -O3 --lossy=75  -k64  ${PWD}/_sources/anim_feerique_site.gif    `seq -f "#%g" 0 2 139` -o ${PWD}/theme/images/stcv_feerique.gif
-giflossy -v -O3 --lossy=150 -k192 ${PWD}/_sources/anim_mongolfiere_site.gif `seq -f "#%g" 0 2 99`  -o ${PWD}/theme/images/stcv_mongolfiere.gif
-giflossy -v -O3 --lossy=200 -k128 ${PWD}/_sources/anim_neige_site.gif       `seq -f "#%g" 0 2 139` -o ${PWD}/theme/images/stcv_neige.gif
-giflossy -v -O3 --lossy=150 -k192 ${PWD}/_sources/anim_ponton_site.gif      `seq -f "#%g" 0 2 139` -o ${PWD}/theme/images/stcv_ponton.gif
-# giflossy -v -O3 --lossy=200 -k192 ${PWD}/_sources/anim_recette_site.gif       -o ${PWD}/theme/images/stcv_recette.gif
+# giflossy -v -O3 --lossy=75  -k64  ${PWD}/_sources/anim_feerique_site.gif    `seq -f "#%g" 0 2 139` -o ${PWD}/theme/images/stcv_feerique.gif
+# giflossy -v -O3 --lossy=150 -k192 ${PWD}/_sources/anim_mongolfiere_site.gif `seq -f "#%g" 0 2 99`  -o ${PWD}/theme/images/stcv_mongolfiere.gif
+# giflossy -v -O3 --lossy=200 -k128 ${PWD}/_sources/anim_neige_site.gif       `seq -f "#%g" 0 2 139` -o ${PWD}/theme/images/stcv_neige.gif
+# giflossy -v -O3 --lossy=150 -k192 ${PWD}/_sources/anim_ponton_site.gif      `seq -f "#%g" 0 2 139` -o ${PWD}/theme/images/stcv_ponton.gif
+giflossy -v -O3 --lossy=200 -k96 ${PWD}/_sources/anim_recette_site.gif `seq -f "#%g" 0 2 79` -o ${PWD}/theme/images/stcv_recette.gif
 
 
 # Anciennes commandes de compression, pour référence si besoin futur
