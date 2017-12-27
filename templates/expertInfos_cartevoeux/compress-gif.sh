c2ab7ed99216448c672ca856811463adbc2346b0
arnaudgaudin <agaudin.lesechos@gmail.com>
2017-12-27 17:11:54 +0100

    Optim param compression

--- templates/expertInfos_cartevoeux/compress-gif.sh
+++ templates/expertInfos_cartevoeux/compress-gif.sh
@@ -15,11 +15,11 @@ echo "compressing gif files ..."
 # Nouvelles CV2018
 giflossy -v -O3 --lossy=800 -k128 ${PWD}/_sources/anim_ambiance.gif    -o ${PWD}/theme/images/nlcv_ambiance.gif
 giflossy -v -O3 --lossy=800 -k128 ${PWD}/_sources/anim_sommet.gif      -o ${PWD}/theme/images/nlcv_sommet.gif
-giflossy -v -O3 --lossy=400 -k128 ${PWD}/_sources/anim_feerique.gif    `seq -f "#%g" 0 3 139` -o ${PWD}/theme/images/nlcv_feerique.gif
+giflossy -v -O3 --lossy=600 -k192 ${PWD}/_sources/anim_feerique.gif    `seq -f "#%g" 0 3 139` -o ${PWD}/theme/images/nlcv_feerique.gif
 giflossy -v -O3 --lossy=600 -k192 ${PWD}/_sources/anim_mongolfiere.gif `seq -f "#%g" 0 2 99`  -o ${PWD}/theme/images/nlcv_mongolfiere.gif
-giflossy -v -O3 --lossy=800 -k128 ${PWD}/_sources/anim_neige.gif       `seq -f "#%g" 0 3 139` -o ${PWD}/theme/images/nlcv_neige.gif
+giflossy -v -O3 --lossy=800 -k192 ${PWD}/_sources/anim_neige.gif       `seq -f "#%g" 0 3 139` -o ${PWD}/theme/images/nlcv_neige.gif
 giflossy -v -O3 --lossy=400 -k192 ${PWD}/_sources/anim_ponton.gif      `seq -f "#%g" 0 3 139` -o ${PWD}/theme/images/nlcv_ponton.gif
-giflossy -v -O3 --lossy=800 -k96  ${PWD}/_sources/anim_recette.gif     `seq -f "#%g" 0 2 79`  -o ${PWD}/theme/images/nlcv_recette.gif
+giflossy -v -O3 --lossy=800 -k128  ${PWD}/_sources/anim_recette.gif     `seq -f "#%g" 0 2 79`  -o ${PWD}/theme/images/nlcv_recette.gif
 
 
 echo "compressing gif files for sites ..."
@@ -33,11 +33,11 @@ echo "compressing gif files for sites ..."
 # Nouvelles CV2018
 giflossy -v -O3 --lossy=400 -k128 ${PWD}/_sources/anim_ambiance_site.gif    -o ${PWD}/theme/images/stcv_ambiance.gif
 giflossy -v -O3 --lossy=400 -k128 ${PWD}/_sources/anim_sommet_site.gif      -o ${PWD}/theme/images/stcv_sommet.gif
-giflossy -v -O3 --lossy=400 -k128  ${PWD}/_sources/anim_feerique_site.gif    `seq -f "#%g" 0 2 139` -o ${PWD}/theme/images/stcv_feerique.gif
+giflossy -v -O3 --lossy=400 -k192  ${PWD}/_sources/anim_feerique_site.gif    `seq -f "#%g" 0 2 139` -o ${PWD}/theme/images/stcv_feerique.gif
 giflossy -v -O3 --lossy=400 -k192 ${PWD}/_sources/anim_mongolfiere_site.gif `seq -f "#%g" 0 2 99`  -o ${PWD}/theme/images/stcv_mongolfiere.gif
-giflossy -v -O3 --lossy=400 -k128 ${PWD}/_sources/anim_neige_site.gif       `seq -f "#%g" 0 2 139` -o ${PWD}/theme/images/stcv_neige.gif
+giflossy -v -O3 --lossy=400 -k192 ${PWD}/_sources/anim_neige_site.gif       `seq -f "#%g" 0 2 139` -o ${PWD}/theme/images/stcv_neige.gif
 giflossy -v -O3 --lossy=400 -k192 ${PWD}/_sources/anim_ponton_site.gif      `seq -f "#%g" 0 2 139` -o ${PWD}/theme/images/stcv_ponton.gif
-giflossy -v -O3 --lossy=400 -k96  ${PWD}/_sources/anim_recette_site.gif     `seq -f "#%g" 0 2 79`  -o ${PWD}/theme/images/stcv_recette.gif
+giflossy -v -O3 --lossy=400 -k128  ${PWD}/_sources/anim_recette_site.gif     `seq -f "#%g" 0 2 79`  -o ${PWD}/theme/images/stcv_recette.gif
 
 
 # Anciennes commandes de compression, pour référence si besoin futur
