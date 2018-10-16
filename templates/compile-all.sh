732970e7dace8b8b3c0ade9bf3651404b2c8acd9
arnaudgaudin <agaudin.lesechos@gmail.com>
2018-10-16 17:37:35 +0200

    Création compilateur pour tout

---
+++ templates/compile-all.sh
@@ -0,0 +1,92 @@
+#compile all BUT ExpertInfos
+echo "compiling all BUT ExpertInfos ..."
+cd agipi_infos_bg_blue
+./compile.sh
+cd ..
+cd agipi_infos_bg_image
+./compile.sh
+cd ..
+cd agipi_infos_bg_white
+./compile.sh
+cd ..
+cd agipi_lettre_news
+./compile.sh
+cd ..
+cd banquepopulaire_liberaletvous
+./compile.sh
+cd ..
+cd bnpparibas_parlonspme
+./compile.sh
+cd ..
+cd booster_health
+./compile.sh
+cd ..
+cd booster_tech
+./compile.sh
+cd ..
+cd expertcom_avocat_2016
+./compile.sh
+cd ..
+cd expertcom_pfa2015
+./compile.sh
+cd ..
+cd expertcom_pfa2016
+./compile.sh
+cd ..
+cd expertcom_referencement_2013
+./compile.sh
+cd ..
+cd expertcom_referencement_2016
+./compile.sh
+cd ..
+cd expertcom_referencement_2016_2col
+./compile.sh
+cd ..
+cd expertcom_referencement_2016_listing
+./compile.sh
+cd ..
+cd expertcom_secteurs_201606
+./compile.sh
+cd ..
+cd gan_infosexpert_2015
+./compile.sh
+cd ..
+cd gan_infosexpert_2016
+./compile.sh
+cd ..
+cd gan_rendezvousexpertpro
+./compile.sh
+cd ..
+cd gan_suiviexpertpro_2014
+./compile.sh
+cd ..
+cd gan_suiviexpertpro_2015
+./compile.sh
+cd ..
+cd gan_suiviexpertpro_2016
+./compile.sh
+cd ..
+cd gan_suiviexpertpro_2017
+./compile.sh
+cd ..
+cd gan_suiviexpertpro_2017_notip
+./compile.sh
+cd ..
+cd orange_diagonales_2018_version1
+./compile.sh
+cd ..
+cd postaExpert_IM025089_version1
+./compile.sh
+cd ..
+cd postaExpert_IM025089_version2
+./compile.sh
+cd ..
+cd postaExpert_IM025089_version3
+./compile.sh
+cd ..
+cd postaExpert_base
+./compile.sh
+cd ..
+cd small_business_tech
+./compile.sh
+cd ..
